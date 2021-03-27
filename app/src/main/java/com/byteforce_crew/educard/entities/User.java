a1b915283af7cc51e42ac9ecc0c8add0aa56f0de
Tracy <57853991+tracyling98@users.noreply.github.com>
2021-03-27 15:04:17 +0800

    Major Edit: Change variables, constructor params.

--- app/src/main/java/com/byteforce_crew/educard/entities/User.java
+++ app/src/main/java/com/byteforce_crew/educard/entities/User.java
@@ -4,42 +4,43 @@ import androidx.annotation.NonNull;
 
 import com.byteforce_crew.educard.enums.Country;
 import com.byteforce_crew.educard.enums.Gender;
-import com.byteforce_crew.educard.enums.Member_Type;
 import com.byteforce_crew.educard.enums.User_Type;
 
 public class User {
 
-    // User Properties
-    protected String username;
+    // -- User Properties --
+    protected String username; /* Document ID */
     protected String password;
     protected String firstname;
     protected String lastname;
+    protected String contact_number;
     protected User_Type user_type;
     protected Gender gender;
     protected Country nationality;
-    protected Member_Type member_type; // Full time or Part time
 
-    // Default Constructor
+
+    // -- Default Constructor --
     public User() {}
 
-    // Other Constructors (Overloading)
-    public User(String username, String password, String firstname, String lastname, User_Type user_type, Gender userGender,
-                Country nationality, Member_Type member_type){
+    // -- Other Constructors (Overloading) --
+    public User(String username, String password, String firstname, String lastname, String contact_number, User_Type user_type, Gender userGender,
+                Country nationality){
         this.username = username;
         this.password = password;
         this.firstname = firstname;
         this.lastname = lastname;
+        this.contact_number = contact_number;
         this.user_type = user_type;
         this.gender = userGender;
         this.nationality = nationality;
-        this.member_type = member_type;
     }
 
     public User(User user){
-        this(user.username, user.password, user.firstname, user.lastname, user.user_type, user.gender, user.nationality, user.member_type);
+        this(user.username, user.password, user.firstname, user.lastname, user.contact_number,
+                user.user_type, user.gender, user.nationality);
     }
 
-    // Getters and Setters
+    // -- Getters --
     public String getUsername(){
         return username;
     }
@@ -56,6 +57,10 @@ public class User {
         return lastname;
     }
 
+    public String getContact_number(){
+        return contact_number;
+    }
+
     public User_Type getUserType(){
         return user_type;
     }
@@ -68,13 +73,10 @@ public class User {
         return nationality;
     }
 
-    public Member_Type getMember_type(){
-        return member_type;
-    }
-
+    // -- Setters --
     public void setUsername(String username){
 
-        // Include some validation before storing the values
+        // -- Include some validation before storing the values --
         this.username = username;
     }
 
@@ -90,6 +92,10 @@ public class User {
         this.lastname = lastname;
     }
 
+    public void setContact_number(String contact_number){
+        this.contact_number = contact_number;
+    }
+
     public void setUserType(User_Type user_type){
         this.user_type = user_type;
     }
@@ -102,13 +108,10 @@ public class User {
         this.nationality = nationality;
     }
 
-    public void setJoin_date(Member_Type member_type){
-        this.member_type = member_type;
-    }
-
+    // -- ToString Method --
     @NonNull
     public String toString(){
-        return String.format("Username: %s\nFirstname: %s\nLastname: %s\nUser Type: %s\nGender: %s\nNationality: %s\nMember Type: %s\n",
-                username, firstname, lastname, user_type, gender, nationality, member_type);
+        return String.format("Username: %s\nFirstname: %s\nLastname: %s\nUser Type: %s\nGender: %s\nNationality: %s\n",
+                username, firstname, lastname, user_type, gender, nationality);
     }
 }
