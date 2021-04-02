452466760dbad4a859429e1fb28c35ea8178511d
Tracy <57853991+tracyling98@users.noreply.github.com>
2021-04-02 14:21:18 +0800

    Add fragments for navigation

---
+++ app/src/main/java/com/byteforce_crew/educard/boundaries/EnrolledModulesFragment.java
@@ -0,0 +1,22 @@
+package com.byteforce_crew.educard.boundaries;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.byteforce_crew.educard.R;
+
+public class EnrolledModulesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_enrolled_modules, container, false);
+    }
+}
